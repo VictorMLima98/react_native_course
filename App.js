7fc8a9b3ea8a81ac27bb3a38293509c4e5935bf4
Victor Lima <victor.lima@devsquad.com>
2022-12-23 23:58:05 -0300

    Style the goal items, append them in the array

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Button, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+	const [newGoal, setNewGoal] = useState('');
+	const [goals, setGoals] = useState([]);
+
+	function goalInputHandler(text) {
+		setNewGoal(text);
+	};
+
+	function addGoalHandler() {
+		setGoals(currentGoals => [
+			...currentGoals,
+			newGoal
+		]);
+	};
+
 	return (
 		<View style={
 			styles.appContainer
@@ -9,13 +24,20 @@ export default function App() {
 			<StatusBar style='auto' />
 
 			<View style={styles.inputContainer}>
-				<TextInput style={styles.textInput} placeholder="Add a Goal..." />
+				<TextInput
+					style={styles.goalInput}
+					placeholder="Add a Goal..."
+					onChangeText={goalInputHandler} />
 
-				<Button title="Add" />
+				<Button title="Add" onPress={addGoalHandler} />
 			</View>
 
-			<View style={styles.listContainer}>
-				<Text>Your goals so far:</Text>
+			<View style={styles.goalsContainer}>
+				<Text style={{
+					fontSize: 20,
+					marginBottom: 8
+				}}>Your goals so far:</Text>
+				{goals.map((goal, index) => <Text style={styles.goalItem} key={index}>{goal}</Text>)}
 			</View>
 		</View>
 	);
@@ -33,21 +55,26 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		alignItems: 'center',
-		marginBottom: 24,
-		borderBottomWidth: 1,
-		borderBottomColor: '#ccc'
 	},
 
-	textInput: {
+	goalInput: {
 		flex: 1,
 		borderWidth: 1,
 		borderColor: '#ccc',
 		borderRadius: 6,
 		marginRight: 8,
-		padding: 8
+		padding: 8,
+		fontSize: 16,
 	},
 
-	listContainer: {
-		flex: 8,
+	goalsContainer: {
+		flex: 10,
+	},
+
+	goalItem: {
+		paddingVertical: 24,
+		marginVertical: 3,
+		backgroundColor: '#eee',
+		fontSize: 16
 	}
 });
