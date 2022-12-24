95fb9f27099433a5c6d04fde147b10815cd81d21
Victor Lima <victor.lima@devsquad.com>
2022-12-23 23:14:19 -0300

    Section 2 - Class 20

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
+		<View style={
+			styles.container
+		}>
+			<StatusBar style='auto' />
+
+			<View style={styles.inputContainer}>
+				<TextInput style={styles.textInput} placeholder="Add a Goal..." />
+				<Button title="Add" />
+			</View>
+
+			<View>
+				<Text>Your goals so far:</Text>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		paddingTop: 34,
+		paddingHorizontal: 8
 	},
+
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: "space-between",
+		alignItems: 'center',
+		marginBottom: 8
+	},
+
+	textInput: {
+		flex: 1,
+		borderWidth: 1,
+		borderColor: '#ccc',
+		borderRadius: 6,
+		marginRight: 8,
+		padding: 8
+	}
 });
